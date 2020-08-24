380fd9273aa3435eda7346bf5aaa16abe8e92d79
Tia TeHiko <tiatehiko@gmail.com>
2020-08-25 11:00:09 +1200

    add changeColor on click event

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -4,23 +4,24 @@ const randomHexColor = () =>
   `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
 class Pixel extends React.Component {
-  constructor(props) {
-    super(props)
-    
 
-    this.state = {
-      style: {
-        width: props.width,
-        height: props.height,
+  state = {
+        width: this.props.width,
+        height: this.props.height,
         backgroundColor: randomHexColor()
-      }
-    }
+  }
+
+  changeColor = event => {
+      this.setState ({
+          backgroundColor: randomHexColor()
+      })
   }
 
     render () {
+        const {width, height, backgroundColor} = this.state
+        const style = {width, height, backgroundColor}
         return (
-            <div style={this.state.style}>
-
+            <div style={style} onClick={this.changeColor}>
             </div>
         )
     }
