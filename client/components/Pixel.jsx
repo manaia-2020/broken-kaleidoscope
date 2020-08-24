cb287ab96cb6e8e989079d4e13bd46b27c4b8852
CHURCH-GIRLS <evapilot.808@gmail.com>
2020-08-25 11:42:36 +1200

    alot-of-colorful-SQUARES-work[C

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 
+const randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
 class Pixel extends React.Component{
+   
+    state = {
+        height: '50px',
+        width: '50px',
+        backgroundColor: randomHexColor()
+    }
+
+    rainbowPixel = () =>{
+        this.setState({
+            backgroundColor: randomHexColor()
+        })
+    }
+
     render(){
         return(
-            <div style={{
-                height: '100px',
-                width: '100px',
-                backgroundColor: 'cornflowerblue',
-              }}>
-                  
+            <div style={this.state}>   
             </div>
         )
     }
