39d8354f8ae0d4648b85287ca0d3bbc2130ae9dd
Shane Chapman <shane@Shanes-MacBook-Pro.local>
2020-08-25 11:38:03 +1200

    completed the onclick change color function

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,12 +1,11 @@
 import React, {Component} from "react"
 
-
+const randomHexColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
 class Pixel extends Component {
     constructor(props) {
         super(props)
-           const randomHexColor = () =>
-              `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
             
         this.state = {
             style : {
@@ -16,11 +15,22 @@ class Pixel extends Component {
             }
         }
     }
+    clickHandler = evt => {
+        this.setState({
+          style: {
+            height: '200px',
+            width: '200px',
+            backgroundColor: randomHexColor()
+          }
+        })
+      }
         render() {
            return(
-                <div style={this.state.style}>
-                </div>
+                <div style={this.state.style} 
+                onClick={() => this.clickHandler()}/>
+                
            )
+        
         }
 }
 
