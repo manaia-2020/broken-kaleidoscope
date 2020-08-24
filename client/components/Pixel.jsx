c91173f4a87e179df494eacc21f74468251fc617
Emma <student@emma-matthews-mac-desktop.hub>
2020-08-25 11:44:20 +1200

    add random color

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 class Pixel extends React.Component {
+  randomHexColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
   state = {
-    style: { height: 200, width: 200, backgroundColor: 'cornflowerblue' },
+    style: { height: 200, width: 200, backgroundColor: this.randomHexColor() },
   };
 
   render() {
