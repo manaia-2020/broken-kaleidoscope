b3c1d159fd3ccf4a583452963b5b89fff0ab8e1f
Emma <student@emma-matthews-mac-desktop.hub>
2020-08-25 10:42:43 +1200

    add style to pixel

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pixel = () => {
+  return (
+    <div style={{
+      height: 200,
+      width: 200,
+      backgroundColor: "cornflowerblue"
+    }}> </div>
+  )
+}
+
+export default Pixel
