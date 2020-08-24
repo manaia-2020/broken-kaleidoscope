622e278e53b4d2f872312d57427dc3c15d2e8150
CHURCH-GIRLS <evapilot.808@gmail.com>
2020-08-25 10:44:54 +1200

    blue-SQUARE-works

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Pixel extends React.Component{
+    render(){
+        return(
+            <div style={{
+                height: '100px',
+                width: '100px',
+                backgroundColor: 'cornflowerblue',
+              }}>
+                  
+            </div>
+        )
+    }
+}
+
+
+
+
+export default Pixel
