ccc9265599189ab4f91fc6fde875a32d16fd8a02
Shane Chapman <shane@Shanes-MacBook-Pro.local>
2020-08-25 11:19:59 +1200

    Add Kaleidescope Colours

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -5,12 +5,14 @@ import React, {Component} from "react"
 class Pixel extends Component {
     constructor(props) {
         super(props)
-
+           const randomHexColor = () =>
+              `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+            
         this.state = {
             style : {
                 height: '200px',
                 width: '200px',
-                backgroundColor: 'blue'
+                backgroundColor: randomHexColor()
             }
         }
     }
