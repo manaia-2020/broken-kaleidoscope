e7201805e15d8e9c2a4b5c7b9fbecf7d395ecbf3
Tia TeHiko <tiatehiko@gmail.com>
2020-08-25 16:45:51 +1200

    add prompts to props

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const App = (props) => {
+  const width = props.width + 'px'
+  const height = props.height + 'px'
+
   return (
     <div>
-      {Array.from({length: props.length}, (v, i) => <Pixel key={i} width='30px' height='30px' />)}
+      {Array.from({length: props.length}, (v, i) => <Pixel key={i} width={width} height={height} />)}
     </div>
   )
 }
--- client/index.js
+++ client/index.js
@@ -4,8 +4,12 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const length = prompt('Enter length: ', 1000)
+  const width = prompt('Enter width in pixels: ', 20)
+  const height = prompt('Enter height in pixels: ', 20)
+
   ReactDOM.render(
-    <App length='1000' />,
+    <App length={length} width={width} height={height} />,
     document.getElementById('app')
   )
 })
