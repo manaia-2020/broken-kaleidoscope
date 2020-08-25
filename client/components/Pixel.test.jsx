3e5bc9375c5fd67bcc40ed8d3af94282fea8c0aa
Shane Chapman <shane@Shanes-MacBook-Pro.local>
2020-08-25 16:13:12 +1200

    Updating Pixel Test

---
+++ client/components/Pixel.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {render, screen, fireEvent} from '@testing-library/react'
+
+import Pixel from './Pixel'
+
+describe('Pixel', () =>{
+
+
+    test('testing if the colors change with left click', () =>{
+        
+    })
+})
