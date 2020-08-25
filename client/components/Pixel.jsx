adc0a67e5ebe093885a0f60996fff82fab5f2447
aidan-starke <starke.aidan@gmail.com>
2020-08-25 12:00:44 +1200

    add setInterval

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,45 +1,89 @@
 import React from 'react'
 
+const width = 40
+const height = width
+const randomColour = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`
+
 class Pixel extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             style: {
-                height: '40px',
-                width: '40px',
-                background: this.randomHexColor()
+                height,
+                width,
+                background: randomColour()
             }
         }
+        setInterval(this.setRandomColour, 2000)
     }
 
     clickHandler = (event) => {
-        this.setState({
-            style: {
-                height: '40px',
-                width: '40px',
-                background: this.randomHexColor()
-            }
-        })
+        this.setRandomColour()
     }
 
     clickHandlerGreen = (event) => {
-        this.setState ({
+        this.setState({
             style: {
-                height: '40px',
-                width: '40px',
+                height,
+                width,
                 background: 'green'
             }
         })
     }
 
-    randomHexColor = () => {
-        return `#${Math.floor(Math.random() * 0x1000000)
-            .toString(16).padStart(6, 0)}`
+    contextHandler = (event) => {
+        event.preventDefault()
+        this.setState({
+            style: {
+                height,
+                width,
+                background: 'black'
+            }
+        })
     }
 
+    doubleClickHandler = event => {
+        this.setState({
+            style: {
+                height,
+                width,
+                background: 'white'
+            }
+        })
+    }
+
+    onDragHandler = event => {
+        this.setState({
+            style: {
+                height,
+                width,
+                background: 'yellow'
+            }
+        })
+    }
+
+    componentDidMount = () => {
+
+    }
+
+    setRandomColour = () => {
+        this.setState({
+            style: {
+                height,
+                width,
+                background: randomColour()
+            }
+        })
+    }
+
+
     render() {
+        const { style } = this.state
         return (
-            <div style={this.state.style} onClick={this.clickHandler} onMouseEnter={this.clickHandlerGreen}>
+            <div style={style} onClick={this.clickHandler}
+                onMouseEnter={this.clickHandlerGreen} onContextMenu={this.contextHandler}
+                onDoubleClick={this.doubleClickHandler} onDragEnter={this.onDragHandler}
+                onLoad={this.intervalStart}>
 
             </div>
         )
@@ -47,3 +91,6 @@ class Pixel extends React.Component {
 }
 
 export default Pixel
+
+// https://stackoverflow.com/questions/41229332/react-uncaught-rangeerror-maximum-call-stack-size-exceeded
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Too_much_recursion
