da3718393f60899652a87ba273a2e883d1b0bf79
James-Prentice <Jamesprenticez@gmail.com>
2020-08-25 15:21:54 +1200

    OnClick event handler working

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
-
-const randomColour = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`
+const randomColour = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
 class Pixel extends React.Component {
 
@@ -11,21 +10,33 @@ class Pixel extends React.Component {
         backgroundColor: randomColour(),
     }
 
+    clickHandler = (event) => {
+        this.setState({        
+            backgroundColor: randomColour()
+        })
+    }
+
     render() {
         return (
             <div style={{
                 height: this.state.height,
                 width: this.state.width,
                 backgroundColor: this.state.backgroundColor,
-            }}>
+            }}
+            onMouseOver={() => this.clickHandler()}>
             </div>
         )
     }
 }
 
+
 export default Pixel
 
-//https://github.com/manaia-2020/broken-kaleidoscope/blob/solution/client/components/Pixel.jsx
+// https://github.com/manaia-2020/broken-kaleidoscope/blob/solution/client/components/Pixel.jsx
 
 
-
+// clickHandler = evt => {
+//     this.setState({
+//       backgroundColor: randomColour()
+//     })
+//   }
