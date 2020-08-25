b0c1bfbb4def229a8db00c65dd702d4a30b84937
Your Namemax-remacle <remacle.maxime@gmail.com>
2020-08-25 15:26:26 +1200

    add event listners

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,48 +1,73 @@
-import React from 'react'
+import React from "react";
 
-const randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000)
+    .toString(16)
+    .padStart(6, 0)}`;
 
-class Pixel extends React.Component{
-   
-    state = {
-        height: '50px',
-        width: '50px',
-        backgroundColor: randomHexColor()
-    }
+class Pixel extends React.Component {
+  state = {
+    height: "50px",
+    width: "50px",
+    backgroundColor: randomHexColor(),
+  };
 
-    clickHandler = evt => {
-        this.setState({
-         backgroundColor: randomHexColor()
-        })
-      }
+  clickHandler = (evt) => {
+    this.setState({
+      backgroundColor: randomHexColor(),
+    });
+  };
 
-    mouseEnterHandler = evt =>{
-        this.setState({
-            backgroundColor:"green"
-        })
-    }
+  mouseEnterHandler = (evt) => {
+    this.setState({
+      backgroundColor: "green",
+    });
+  };
 
-    onMouseOut = evt =>{
-        this.setState({
-            backgroundColor: randomHexColor()
-        })
-    }
+  onMouseOut = (evt) => {
+    this.setState({
+      backgroundColor: randomHexColor(),
+    });
+  };
 
-    rainbowPixel = () =>{
-        this.setState({
-            backgroundColor: randomHexColor()
-        })
-    }
+  rainbowPixel = () => {
+    this.setState({
+      backgroundColor: randomHexColor(),
+    });
+  };
 
-    render(){
-        return(
-            <div style={this.state}onClick={this.clickHandler} onMouseEnter={this.mouseEnterHandler} onMouseOut={this.onMouseOut}>   
-            </div>
-        )
-    }
+  rightClickHandler = (evt) => {
+    evt.preventDefault()
+    this.setState({
+      backgroundColor: "black",
+    });
+  };
+
+  doubleClickHandler = (evt) =>{
+    this.setState({
+        backgroundColor: "white",
+    });
+  }
+
+  dragEnterHandler = (evt) =>{
+    this.setState({
+        backgroundColor: "yellow",
+    });
+  }
+
+  render() {
+    return (
+      <div 
+        style={this.state}
+        onClick={this.clickHandler}
+        onContextMenu={this.rightClickHandler}
+        onMouseEnter={this.mouseEnterHandler}
+        onMouseOut={this.onMouseOut}
+        onDoubleClick={this.doubleClickHandler}
+        onDragEnter = {this.dragEnterHandler}
+      ></div>
+    );
+  }
 }
 
-
-
-
-export default Pixel
+export default Pixel;
