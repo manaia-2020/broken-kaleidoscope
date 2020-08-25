39ca256365a968158a1f86db64a32c2e07706532
Tia TeHiko <tiatehiko@gmail.com>
2020-08-25 16:02:15 +1200

    hover test

--- client/components/Pixel.test.jsx
+++ client/components/Pixel.test.jsx
@@ -3,11 +3,17 @@ import { render, screen, fireEvent } from '@testing-library/react'
 import Pixel from './Pixel'
 
 describe('<Pixel />', () => {
-  test('check if the colour changes on click', () => {
+  test('check if the colour changes on hover', () => {
     render(<Pixel width='20px' height='20px' />)
 
-    const button = screen.getAllByRole('button', {hidden: true})
+    const button = screen.getByRole('button', {hidden: true})
 
+    const previousColour = button.style.backgroundColor
+    fireEvent.mouseEnter(button)
+    const colour = button.style.backgroundColor
 
+    expect.assertions(2)
+    expect(colour).not.toBe(previousColour)
+    expect(colour).toContain('white')
   })
 })
