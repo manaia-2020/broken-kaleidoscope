1b133ac08cf69b0e451610db2ef0a75f7fbcf206
Bradley Eggins <brad.eggins@gmail.com>
2020-08-25 15:11:51 +1200

    Add Pixel.test.js

---
+++ client/components/Pixel.test.jsx
@@ -0,0 +1,7 @@
+import {render,getByRole, fireEvent } from '@testing-library/react'
+
+describe('Test Pixel Component', () => {
+    test('Check test setup working', () => {
+        expect(true).toBeTruthy()
+    })
+})
