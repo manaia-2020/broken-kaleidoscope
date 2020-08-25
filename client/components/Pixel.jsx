5032c8d6817d4f2109247916f5c4e64a59f76ba2
Emma Slight <emma.slight@icloud.com>
2020-08-25 15:50:26 +1200

    create a makeblack on rightclick funtion

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -4,9 +4,6 @@ const randomHexColor = () =>
     .toString(16)
     .padStart(6, 0)}`;
 class Pixel extends React.Component {
-  randomHexColor = () =>
-    `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-
   state = {
     width: this.props.width,
     height: this.props.height,
@@ -21,14 +18,31 @@ class Pixel extends React.Component {
 
   makeGreen = (event) => {
     this.setState({
-      backgroundColor: 'green'
+      backgroundColor: 'green',
+    });
+  };
 
-    })
+  makeBlack = (e) => {
+    e.preventDefault();
+    this.setState({
+      backgroundColor: 'black',
+    });
+  };
 
-  }
   render() {
-    const style = { width: 50, height: 50, backgroundColor: this.state.backgroundColor };
-    return <div onClick={this.changeColor} style={style} onMouseEnter={this.makeGreen}></div>
+    const style = {
+      width: 50,
+      height: 50,
+      backgroundColor: this.state.backgroundColor,
+    };
+    return (
+      <div
+        onClick={this.changeColor}
+        style={style}
+        onMouseEnter={this.makeGreen}
+        onContextMenu={this.makeBlack}
+      ></div>
+    );
   }
 }
 
