543d00f213f7dab38c88f96139cff2ac72766969
Joshua Vial <joshua@enspiral.com>
2020-08-25 15:42:58 +1200

    setting up pixel test

--- client/components/Pixel.test.jsx
+++ client/components/Pixel.test.jsx
@@ -6,8 +6,12 @@ describe('<Pixel />', () => {
   test('check if the colour changes on click', () => {
     render(<Pixel width='20px' height='20px' />)
 
-    const button = screen.getAllByRole('button', {hidden: true})
+    const button = screen.getByRole('button', {hidden: true})
 
+    console.log(button.style.backgroundColor)
+    fireEvent.mouseEnter(button)
+    console.log(button.style.backgroundColor)
 
+    //how would you assert the style has changed?
   })
 })
