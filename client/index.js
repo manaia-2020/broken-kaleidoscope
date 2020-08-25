52c59fe273dcf8a8aa0e4948a12dceacf34c417c
Tia TeHiko <tiatehiko@gmail.com>
2020-08-25 16:25:02 +1200

    add length prop

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <App length='1000' />,
     document.getElementById('app')
   )
 })
